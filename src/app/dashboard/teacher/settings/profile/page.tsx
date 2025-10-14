5d3e1ca81ed9dbc1ae3d726c331da3992d581879
Filson <ajffilson@gmail.com>
2025-10-14 20:04:42 +0100

    site update v3.2

--- src/app/dashboard/teacher/settings/profile/page.tsx
+++ src/app/dashboard/teacher/settings/profile/page.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 import Profile from '@/features/profile/Profile';
 
 const ProfilePage: React.FC = () => {
-    return <Profile title='Profile coming soon' />;
+    return <Profile />;
 }
 
 export default ProfilePage; 
