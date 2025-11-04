ec8b1b0f44315bbab0d79d6d895856fb2b25549a
Filson <ajffilson@gmail.com>
2025-11-04 18:08:13 +0100

    site update v5.2

--- src/app/dashboard/super/lessons/page.tsx
+++ src/app/dashboard/super/lessons/page.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 import Lessons from '@/features/lessons/Lessons';
 
 const LessonsPage: React.FC = () => {
-    return <Lessons title='Lessons coming soon' />;
+    return <Lessons />;
 }
 
 export default LessonsPage;
