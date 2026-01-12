a44580ae9d9acbc4ce977dd3bb1040d7ebd94558
Filson <ajffilson@gmail.com>
2026-01-12 16:10:44 +0100

    changes1.1

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [{ hostname: "images.pexels.com" }],
+    unoptimized: true,
   },
   typescript: {
     ignoreBuildErrors: true,
