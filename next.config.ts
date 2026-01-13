40aa52579bce6e210f71efa0dadebed1303bb3fe
Filson <ajffilson@gmail.com>
2026-01-13 02:17:45 +0100

    site update v7.4.7

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,9 @@
-
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [{ hostname: "images.pexels.com" }],
   },
-  eslint: {
-    // Warning: This will skip *all* lint errors during `next build`
-    ignoreDuringBuilds: true,
-  },
-  /* typescript: {
-    // Warning: This will skip *all* TypeScript errors during `next build`
-    ignoreBuildErrors: true,
-  },
-  experimental: {
-    // Disable static optimization for pages that use dynamic imports
-    optimizePackageImports: [],
-  }, */
 };
 
 export default nextConfig;
-
-
